e242c5a86320182b4dc7a6b1f6d8474a0fbcc219
Ronak Mangroliya <ronakpatel2378@gmail.com>
2024-03-11 23:35:11 +0530

    employee form validation !!

--- src/app/employee-list/employee-add/employee-add.component.ts
+++ src/app/employee-list/employee-add/employee-add.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-add',
@@ -10,12 +11,44 @@ export class EmployeeAddComponent {
   profilePreview!: string | ArrayBuffer | null ; // Property for profile picture preview
   aadhaarPreview!: string | ArrayBuffer | null ;
 
-  constructor() {}
+  employeeForm!: FormGroup;
 
-  onSubmit() {
-    // Handle form submission logic here
-    console.log(this.employee);
+  constructor(private formBuilder: FormBuilder) {
+    this.employeeForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      mobileNumber: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
+      aadhaarNumber: ['', [Validators.required, Validators.pattern('[0-9]{12}')]],
+      address: ['', Validators.required],
+      // aadhaarFront: ['', Validators.required],
+      // aadhaarBack: ['', Validators.required],
+      referenceName: ['', Validators.required],
+      referenceMobileNumber: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
+    });
   }
+
+
+  // Helper function to check if a form control has an error
+  hasError(controlName: string, errorName: string) {
+    return this.employeeForm.controls[controlName].hasError(errorName);
+  }
+  
+  
+  
+  onSubmit() {
+
+    if (this.employeeForm.valid) {
+      const formData = this.employeeForm.value;
+      console.log("Form data:", formData); // Debugging message
+      // Further processing of form data
+    }else {
+      console.log("Form is invalid. Cannot submit."); // Debugging message
+      // Handle invalid form
+    }
+  }
+
+
+
 
   onFileSelected(event: any) {
     // Handle file upload logic for profile picture
@@ -31,6 +64,8 @@ export class EmployeeAddComponent {
       reader.readAsDataURL(file);
     }
   }
+
+
 
   onAadhaarPicSelected(event: any) {
     // Handle file upload logic for Aadhaar card profile picture
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MainComponent } from '../main/main.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { EmployeeListComponent } from '../employee-list/employee-list.component';
@@ -23,6 +23,7 @@ import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
     TableModule,
     ButtonModule,
     ProgressSpinnerModule,
+    ReactiveFormsModule
    
 
   ],
