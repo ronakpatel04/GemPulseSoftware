d45f98c7e47a8179428c9c2eb0614d4a79369bd0
Ronak Mangroliya <ronakpatel2378@gmail.com>
2024-03-19 00:10:56 +0530

    Design issue

--- main.js
+++ main.js
@@ -8,20 +8,11 @@ let win;
 
 function createWindow() {
     win = new BrowserWindow({
-        width: 1717,
-        height: 817,
+        width: 800,
+        height: 600,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
         }
-    });
-
-
-    win.setResizable(true);
-
-
-
-    win.on('maximize', () => {
-        win.setSize(1717, 817); 
     });
 
 
@@ -30,37 +21,24 @@ function createWindow() {
 
     win.on('closed', () => {
         win = null;
-        
     });
     
     
 
 }
 
-function buildAngularProject(angularProjectDirectory) {
-    // Delete the dist directory to ensure a clean build
-    const distDirectory = path.join(angularProjectDirectory, 'dist');
-    if (fs.existsSync(distDirectory)) {
-        fs.rmdirSync(distDirectory, { recursive: true });
-    }
 
-    // Run ng build command
-    exec('ng build --base-href ./', { cwd: angularProjectDirectory }, (err, stdout, stderr) => {
-        if (err) {
-            console.error('Error building Angular project:', err);
-            return;
-        }
-
-        console.log(stdout);
-        console.error(stderr);
-
-        createWindow();
-    });
-}
 
 
 app.on('ready', ()=>{
+    
+    
     createWindow();
+
+    win.webContents.on('did-finish-load', () => {
+        win.webContents.setZoomFactor(0.7);
+      });
+    
 
     Menu.setApplicationMenu(null);
     win.webContents.openDevTools();
@@ -77,6 +55,9 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
     if (win === null) {
         createWindow();
+        win.webContents.on('did-finish-load', () => {
+            win.webContents.setZoomFactor(0.7);
+          });
     }
 });
 
